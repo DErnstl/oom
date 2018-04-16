9bf5ce2112b362b66d88faa3ea76e371777d707e
Dominic Ernstbrunner <ic17b015@technikum-wien.at>
2018-04-16 23:00:46 +0200

    ADD TASK 6

---
+++ tasks/Task6/task6/task4/Push.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Threading;
+using static System.Console;
+
+namespace task6
+{
+
+    public static class PushExampleWithSubject
+    {
+        public static string getRandomName(int length)
+        {
+            char[] name = new char[length];
+            Random random = new Random();
+            char[] vocale = new char[] { 'a', 'e', 'i', 'o', 'u', 'ä', 'ö', 'ü' };
+            char c;
+            for (int i = 0; i < 6; i++)
+                if (i % 2 == 0)
+                {
+                    do c = (char)(random.Next((int)'z' - (int)'a') + (int)'a');
+                    while (Array.IndexOf<char>(vocale, c) >= 0);
+                    name[i] = c;
+                }
+                else
+                    name[i] = vocale[random.Next(vocale.Length)];
+
+            return new string(name);
+        }
+
+        public static void Run()
+        {
+            var source = new Subject<int>();
+            Random rnd = new Random();
+
+            source
+                .Sample(TimeSpan.FromSeconds(1.0))
+                .Subscribe(x => WriteLine($"received {x}"))
+                ;
+
+
+            var t = new Thread(() =>
+            {
+                var i = 0; ;// = new TransferMarket(getRandomName(5), getRandomName(8), rnd.Next(18, 45), getRandomName(8), rnd.Next(0, 100000));
+                while (true)
+                {
+                    Thread.Sleep(250);
+                    source.OnNext(i);
+                    WriteLine($"sent {i}");
+                    //i++;
+                }
+            });
+            t.Start();
+        }
+    }
+}
---
+++ tasks/Task6/task6/task4/Tests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using task6;
+
+namespace Test
+{
+    [TestFixture]
+    class Tests
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            Assert.IsTrue(1 == 1);
+        }
+
+        [Test]
+        public void CheckInitialisationOfAPlayer()
+        {
+            var init = new TransferMarket("ASDF", "FDSA", 55, "FC Test", 123456);
+            Assert.IsTrue(init.Name.PFirstName == "ASDF");
+            Assert.IsTrue(init.Name.PSecondName == "FDSA");
+            Assert.IsTrue(init.Age == 55);
+            Assert.IsTrue(init.Club == "FC Test");
+            Assert.IsTrue(init.MarketValue == 123456);
+        }
+
+        [Test]
+        public void CannotCreateAPlayerWithoutAFirstname1()
+        {
+            Assert.Catch(() =>
+            {
+                var init = new TransferMarket("", "FDSA", 55, "FC Test", 123456);
+            });
+        }
+
+        [Test]
+        public void CannotCreateAPlayerWithoutAFirstname2()
+        {
+            Assert.Catch(() =>
+            {
+                var init = new TransferMarket(null, "FDSA", 55, "FC Test", 123456);
+            });
+        }
+
+        [Test]
+        public void CannotCreateAPlayerWithoutASecondname1()
+        {
+            Assert.Catch(() =>
+            {
+                var init = new TransferMarket("ASDF", "", 55, "FC Test", 123456);
+            });
+        }
+
+        [Test]
+        public void CannotCreateAPlayerWithoutASecondname2()
+        {
+            Assert.Catch(() =>
+            {
+                var init = new TransferMarket("ASDF", null, 55, "FC Test", 123456);
+            });
+        }
+
+        [Test]
+        public void CannotCreateAPlayerWithANegativeAge1()
+        {
+            Assert.Catch(() =>
+            {
+                var init = new TransferMarket("ASDF", "FDSA", -1, "FC Test", 123456);
+            });
+        }
+
+        [Test]
+        public void CannotCreateAPlayerWithAAgeOver200()
+        {
+            Assert.Catch(() =>
+            {
+                var init = new TransferMarket("ASDF", "FDSA", 201, "FC Test", 123456);
+            });
+        }
+
+        [Test]
+        public void CannotCreateAPlayerWithANegativeMarketvalue()
+        {
+            Assert.Catch(() =>
+            {
+                var init = new TransferMarket("ASDF", "FDSA", 55, "FC Test", -1);
+            });
+        }
+
+    }
+}
